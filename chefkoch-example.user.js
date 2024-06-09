c38010f08e2e9738a4b79b480e2a51e5bd76cc6e
WandernderVogel <wanderndervogel@gmail.com>
2024-06-09 16:08:04 +0200

    renamed file for petter use with tampermonkey

---
+++ chefkoch-example.user.js
@@ -0,0 +1,38 @@
+/* TODO
+ *   optimize for mobile:
+ *       delte einkaufsliste and bei rewe bestellen button
+ *       set Zubereitung directly under zutaten
+ *    for recipes in different parts(https://www.chefkoch.de/rezepte/2529831396465550/Pfannkuchen-Crepe-und-Pancake.html):
+ *       make parts expandable
+ *   optimize for pc:
+ *       make Zubereitung rearange into the sidebar
+ *   general:
+ *       Make Browser Addon for Firefox, Chrome, Duckduckgo and when needed others too
+ */
+
+// ==UserScript==
+// @name         Better Chefkoch
+// @namespace    https://github.com/WandernderVogel
+// @version      Alpha-v1
+// @description  Make the UI of Chefkoch.de better on mobile and PC
+// @author       Wandernder Vogel
+// @match        https://chefkoch.de/*
+// @icon         https://www.chefkoch.de/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  ("use strict");
+  // MOBILE
+  document.getElementsByClassName("pi-cont")[0].parentElement.innerHTML = "";
+  document.getElementsByClassName("ds-hr ")[6].innerHTML = "";
+  document.querySelectorAll("guj-ad-outstream")[0].style.minHeight = "0px";
+
+  // delete "Zubereitungstipps on mobile"
+  document.querySelectorAll(
+    '[data-vars-tracking-title="Zubereitungstipps"]'
+  )[0].parentElement.innerHTML = "";
+  document.querySelectorAll(
+    '[data-vars-tracking-title="Zutaten"]'
+  )[0].parentElement.style.marginBottom = "0px";
+})();
